5b19a83af85c3601e726698cd7834f33ce6e3730
Mike Rieser <atlassian@mrieser.com>
2024-12-06 15:33:10 -0600

    release: updated Beta suitable for Release

--- TTS_xwing/src/Game/VersionCheck.lua
+++ TTS_xwing/src/Game/VersionCheck.lua
@@ -1,5 +1,5 @@
-current_version = 99      -- mod version (workshop update count)
-workshop_id = '2486128992'   -- mod workshop ID
+current_version = 100      -- mod version (workshop update count)
+workshop_id = '2486128992' -- mod workshop ID
 
 -- To read above numbers, open your mod workshop page
 --  current_version corresponds to XX in "XX Change Notes (view)"
@@ -13,7 +13,7 @@ function onLoad()
     self.tooltip = false
     self.tooltip = false
     self.lock()
-    self.setPosition({0, -5, 0})
+    self.setPosition({ 0, -5, 0 })
     CheckVersion()
 end
 
@@ -35,17 +35,17 @@ end
 -- Argument being nil means version coudn't be read
 function ReportCheck(workshop_version)
     if not workshop_version then
-        printToAll('Couldn\'t read mod version from Steam Workshop', {0.4, 0.4, 1})
+        printToAll('Couldn\'t read mod version from Steam Workshop', { 0.4, 0.4, 1 })
     elseif workshop_version > current_version then
-        broadcastToAll('A newer version of this mod is available on Steam Workshop!', {1, 0.2, 0.2})
+        broadcastToAll('A newer version of this mod is available on Steam Workshop!', { 1, 0.2, 0.2 })
     elseif workshop_version < current_version then
         -- for mod author convenience
         if current_version - workshop_version == 1 then
-            broadcastToAll('Ready to push to Steam Workshop', {0.2, 1, 1})
+            broadcastToAll('Ready to push to Steam Workshop', { 0.2, 1, 1 })
         else
-            broadcastToAll('Workshop version more than 1 behind mod version?', {1, 0.2, 0.2})
+            broadcastToAll('Workshop version more than 1 behind mod version?', { 1, 0.2, 0.2 })
         end
     else
-        printToAll('Mod is up to date', {0.1, 1, 0.1})
+        printToAll('Mod is up to date', { 0.1, 1, 0.1 })
     end
 end
