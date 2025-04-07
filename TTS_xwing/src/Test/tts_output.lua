ba63ca0b0e31742fe95c188b1ac52650aa12483d
Mike Rieser <atlassian@mrieser.com>
2025-04-07 10:49:11 -0500

    refactor: trying for improvements to the TTSOutput

--- TTS_xwing/src/Test/tts_output.lua
+++ TTS_xwing/src/Test/tts_output.lua
@@ -17,29 +17,29 @@ function TTSOutput.new(runner)
 end
 
 function TTSOutput:startSuite()
-    printToAll("TTO: Starting test suite with " .. self.totalTests .. " tests", {0, 1, 0}) -- Green
+    printToAll("TTO: Starting test suite with " .. self.totalTests .. " tests", { 0, 1, 0 }) -- Green
     UI.setAttribute("TestProgressBar", "percentage", 0)
 end
 
 function TTSOutput:startClass(className)
     if self.verbosity > M.VERBOSITY_LOW then
-        printToAll("TTO: Starting class: " .. className, {1, 1, 0}) -- Yellow
+        printToAll("TTO: Starting class: " .. className, { 1, 1, 0 }) -- Yellow
     end
 end
 
 function TTSOutput:startTest(testName)
     if self.verbosity > M.VERBOSITY_DEFAULT then
-        printToAll("TTO: Starting test: " .. testName, {1, 1, 0}) -- Yellow
+        printToAll("TTO: Starting test: " .. testName, { 1, 1, 0 }) -- Yellow
     end
 end
 
 function TTSOutput:updateStatus(node)
     if node:isSkipped() then
-        printToAll("TTO: Test skipped: " .. node.testName, {0, 0, 1}) -- Blue
+        printToAll("TTO: Test skipped: " .. node.testName, { 0, 0, 1 }) -- Blue
     elseif node:isFailure() then
-        printToAll("TTO: Test failed: " .. node.testName, {1, 0, 0}) -- Red
+        printToAll("TTO: Test failed: " .. node.testName, { 1, 0, 0 }) -- Red
     elseif node:isError() then
-        printToAll("TTO: Test error: " .. node.testName, {1, 0, 0}) -- Red
+        printToAll("TTO: Test error: " .. node.testName, { 1, 0, 0 }) -- Red
     end
 end
 
@@ -47,29 +47,25 @@ function TTSOutput:endTest(node)
     self.completedTests = self.completedTests + 1
     local progress = (self.completedTests / self.totalTests) * 100
     if node:isSuccess() then
-        printToAll("TTO: Test " .. self.completedTests .. " passed", {0, 1, 0}) -- Green
+        printToAll("TTO: Test " .. self.completedTests .. " passed", { 0, 1, 0 }) -- Green
     else
-        printToAll("TTO: Test " .. self.completedTests .. " failed", {1, 0, 0}) -- Red
+        printToAll("TTO: Test " .. self.completedTests .. " failed", { 1, 0, 0 }) -- Red
     end
     UI.setAttribute("TestProgressBar", "percentage", progress)
 end
 
 function TTSOutput:endClass()
     if self.verbosity > M.VERBOSITY_LOW then
-        printToAll("TTO: Ending class", {1, 1, 0}) -- Yellow
+        printToAll("TTO: Ending class", { 1, 1, 0 }) -- Yellow
     end
 end
 
 function TTSOutput:endSuite()
-    printToAll("TTO: Test suite completed", {0, 0, 1}) -- Blue
+    printToAll("TTO: Test suite completed", { 0, 0, 1 })     -- Blue
     UI.setAttribute("TestProgressBar", "percentage", 100)
-    printToAll(M.LuaUnit.statusLine(self.result), {1, 1, 0}) -- Yellow
+    printToAll(M.LuaUnit.statusLine(self.result), { 1, 1, 0 }) -- Yellow
 end
 
--- Setup function to configure LuaUnit with TTSOutput
-local function setup(luaunitModule)
-    -- luaunitModule.LuaUnit.setOutputType('custom', TTSOutput)
-    luaunitModule.LuaUnit.outputType = TTSOutput
-end
+M.LuaUnit.outputType = TTSOutput
 
-return { setup = setup, TTSOutput = TTSOutput }
+return M
