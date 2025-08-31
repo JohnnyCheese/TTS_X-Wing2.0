ca4bd8ccb6d6292fbb4b44ec83555f456137f56c
Mike Rieser <atlassian@mrieser.com>
2025-08-31 13:47:06 -0500

    chore: bumped version check value.

--- TTS_xwing/src/Game/VersionCheck.lua
+++ TTS_xwing/src/Game/VersionCheck.lua
@@ -1,4 +1,4 @@
-current_version = 112      -- mod version (workshop update count)
+current_version = 113      -- mod version (workshop update count)
 workshop_id = '2486128992' -- mod workshop ID
 
 -- To read above numbers, open your mod workshop page
