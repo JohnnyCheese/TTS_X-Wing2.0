8bc09fdf2a36ca99c5ff431465cffc6b50d52571
Mike Rieser <atlassian@mrieser.com>
2025-08-31 20:09:50 -0500

    feat: replaced duplicate image management code with ImageManager.lua

--- TTS_xwing/src/Game/Table/Epic-Playmat.lua
+++ TTS_xwing/src/Game/Table/Epic-Playmat.lua
@@ -9,8 +9,6 @@
 -- Just add their links to the table below in the same fashion
 
 -- Table of all images to be cycled through with NextImage()
-require("TTS_lib.Util.Table")
-
 imageSet = {
     'https://i.imgur.com/aCxRouI.jpg',
     'https://i.imgur.com/X3luGQr.jpg',
@@ -26,6 +24,21 @@ __XW_MatLayout = 'Epic'
 -- This mat identifier
 __XW_MatID = 'Main'
 
+local ImageManager = require("Game.Component.Playmat.ImageManager")
+local mgr
+
+function onLoad(saved)
+    mgr = ImageManager.install(self, { images = imageSet })
+    mgr:onLoad(saved)
+    self.lock()
+    self.interactable = false
+    self.tooltip = false
+end
+
+function onSave()
+    return mgr:onSave()
+end
+
 local r_sz = { height = 10.83691788, thickness = 0.08344745636, width = 0.5535750389 }
 local corrScale = { 0.625, 0.625, 0.625 }
 --local corrScale = { -2.625, 0.625, 0.625 }
@@ -38,127 +51,6 @@ local rulerData = {
 }
 local R = r_sz.height
 local range_1 = R / 3
-
--- image switching
-local currImage = 1
-
-local function trim(s)
-    return (s:gsub('^%s+', ''):gsub('%s+$', ''))
-end
-
-local function persist()
-    self.script_state = JSON.encode({ curr = currImage, images = imageSet })
-end
-
-function onSave()
-    return self.script_state or JSON.encode({ curr = currImage, images = imageSet })
-end
-
-function onLoad(saved)
-    local s = (type(saved) == "string" and saved ~= "") and saved or self.script_state
-
-    if type(s) == "string" and s:sub(1, 1) == "{" then
-        local t = JSON.decode(s)
-        if type(t.images) == "table" and #t.images > 0 then
-            imageSet = t.images
-        end
-        currImage = tonumber(t.curr) or 1
-    else
-        -- legacy numeric state (pre-JSON), or nothing
-        currImage = tonumber(s) or 1
-    end
-
-    if currImage < 1 then
-        currImage = 1
-    end
-    if currImage > #imageSet then
-        currImage = #imageSet
-    end
-
-    self.lock()
-    self.interactable = false
-    self.tooltip = false
-end
-
-local function changeImage(idx)
-    local custom = self.getCustomObject()
-    custom.diffuse = imageSet[idx]
-    self.setCustomObject(custom)
-    currImage = idx
-    persist()
-    self.reload()
-end
-
-function NextImage()
-    deleteAllEpic()
-    currImage = (currImage % #imageSet) + 1
-    changeImage(currImage)
-end
-
-function PrevImage()
-    deleteAllEpic()
-    currImage = ((currImage - 2) % #imageSet) + 1
-    changeImage(currImage)
-end
-
-function addImage(arg)
-    local url = (type(arg) == 'table') and (arg.url or arg[1]) or arg
-    if type(url) ~= 'string' then
-        return false, 'addImage: missing url'
-    end
-    url = trim(url)
-
-    local idx = table.index_of(imageSet, url) -- from Table.lua
-    if not idx then
-        table.insert(imageSet, url)
-
-        idx = #imageSet
-    end
-
-    if type(deleteAll) == 'function' then
-        deleteAll()
-    end
-    currImage = idx
-    changeImage(currImage)
-    return true, currImage
-end
-
-function removeImage(arg)
-    local idx
-    local url
-    if type(arg) == 'number' then
-        idx = math.floor(arg)
-    else
-        url = (type(arg) == 'table') and (arg.idx and nil or (arg.url or arg[1])) or arg
-        if type(url) == 'string' then
-            idx = table.index_of(imageSet, trim(url))
-        end
-    end
-
-    if not idx or idx < 1 or idx > #imageSet then
-        printToAll('removeImage: url not found: ' .. tostring(url), Color.Red)
-    end
-
-    if not idx or idx < 1 or idx > #imageSet then
-        return false, 'removeImage: not found'
-    end
-    if #imageSet == 1 then
-        return false, 'removeImage: cannot remove the last image'
-    end
-
-    table.remove(imageSet, idx)
-    if idx < currImage then
-        currImage = currImage - 1
-    elseif idx == currImage then
-        currImage = ((currImage - 2) % #imageSet) + 1
-    end
-
-    if type(deleteAll) == 'function' then
-        deleteAll()
-    end
-    changeImage(currImage)
-    return true, currImage
-end
 
 local function geomEpic()
     local b = self.getBoundsNormalized()
--- TTS_xwing/src/Game/Table/HotAC-Playmat.lua
+++ TTS_xwing/src/Game/Table/HotAC-Playmat.lua
@@ -9,8 +9,6 @@
 -- Just add their links to the table below in the same fashion
 
 -- Table of all images to be cycled through with NextImage()
-require("TTS_lib.Util.Table")
-
 imageSet = {
     'https://i.imgur.com/6IkNucB.jpg',
     'https://steamusercontent-a.akamaihd.net/ugc/1483326815456337025/EB436BA5C3D1B0BAF4ED3325456E7211A669E3BF/',
@@ -40,6 +38,21 @@ __XW_MatLayout = 'HotAC'
 -- This mat identifier
 __XW_MatID = 'Main'
 
+local ImageManager = require("Game.Component.Playmat.ImageManager")
+local mgr
+
+function onLoad(saved)
+    mgr = ImageManager.install(self, { images = imageSet })
+    mgr:onLoad(saved)
+    self.lock()
+    self.interactable = false
+    self.tooltip = false
+end
+
+function onSave()
+    return mgr:onSave()
+end
+
 local r_sz = { height = 10.83691788, thickness = 0.08344745636, width = 0.5535750389 }
 local corrScale = { 0.625, 0.625, 0.625 }
 local rulers, rulersState = {}, 0
@@ -52,128 +65,6 @@ local rulerData = {
 local R = r_sz.height
 local range_1 = R / 3
 
--- image switching
-local currImage = 1
-
-local function trim(s)
-    return (s:gsub('^%s+', ''):gsub('%s+$', ''))
-end
-
--- Single source of truth: JSON in self.script_state
-local function persist()
-    self.script_state = JSON.encode({ curr = currImage, images = imageSet })
-end
-
-function onSave()
-    -- Let TTS save exactly what we already keep in script_state
-    return self.script_state or JSON.encode({ curr = currImage, images = imageSet })
-end
-
-function onLoad(saved)
-    local s = (type(saved) == "string" and saved ~= "") and saved or self.script_state
-    
-    if type(s) == "string" and s:sub(1, 1) == "{" then
-        local t = JSON.decode(s)
-        if type(t.images) == "table" and #t.images > 0 then
-            imageSet = t.images
-        end
-        currImage = tonumber(t.curr) or 1
-    else
-        -- legacy numeric state (pre-JSON), or nothing
-        currImage = tonumber(s) or 1
-    end
-    
-    if currImage < 1 then
-        currImage = 1
-    end
-    if currImage > #imageSet then
-        currImage = #imageSet
-    end
-    
-    self.lock()
-    self.interactable = false
-    self.tooltip = false
-end
-
-local function changeImage(idx)
-    local custom = self.getCustomObject()
-    custom.diffuse = imageSet[idx]
-    self.setCustomObject(custom)
-    currImage = idx
-    persist()
-    self.reload()
-end
-
-function NextImage()
-    deleteAll()
-    currImage = (currImage % #imageSet) + 1
-    changeImage(currImage)
-end
-
-function PrevImage()
-    deleteAll()
-    currImage = ((currImage - 2) % #imageSet) + 1
-    changeImage(currImage)
-end
-
-function addImage(arg)
-    local url = (type(arg) == 'table') and (arg.url or arg[1]) or arg
-    if type(url) ~= 'string' then
-        return false, 'addImage: missing url'
-    end
-    url = trim(url)
-    
-    local idx = table.index_of(imageSet, url) -- from Table.lua
-    if not idx then
-        table.insert(imageSet, url)
-        idx = #imageSet
-    end
-    
-    if type(deleteAll) == 'function' then
-        deleteAll()
-    end
-    currImage = idx
-    changeImage(currImage)
-    return true, currImage
-end
-
-function removeImage(arg)
-    local idx
-    local url
-    if type(arg) == 'number' then
-        idx = math.floor(arg)
-    else
-        url = (type(arg) == 'table') and (arg.idx and nil or (arg.url or arg[1])) or arg
-        if type(url) == 'string' then
-            idx = table.index_of(imageSet, trim(url))
-        end
-    end
-    
-    if not idx or idx < 1 or idx > #imageSet then
-        printToAll('removeImage: url not found: ' .. tostring(url), Color.Red)
-    end
-    
-    if not idx or idx < 1 or idx > #imageSet then
-        return false, 'removeImage: not found'
-    end
-    if #imageSet == 1 then
-        return false, 'removeImage: cannot remove the last image'
-    end
-    
-    table.remove(imageSet, idx)
-    if idx < currImage then
-        currImage = currImage - 1
-    elseif idx == currImage then
-        currImage = ((currImage - 2) % #imageSet) + 1
-    end
-    
-    if type(deleteAll) == 'function' then
-        deleteAll()
-    end
-    changeImage(currImage)
-    return true, currImage
-end
-
 local function geom()
     local b = self.getBoundsNormalized()
     local C = Vector(b.center.x, 1.0, b.center.z)
@@ -182,11 +73,11 @@ local function geom()
     local TR = Vector(C.x + halfx, C.y, C.z + halfz)
     local BL = Vector(C.x - halfx, C.y, C.z - halfz)
     local BR = Vector(C.x + halfx, C.y, C.z - halfz)
-    
+
     local halfHeight, halfWidth = R / 2, r_sz.width / 2
     local xa, za = Vector(R, 0, 0), Vector(0, 0, R)
     local xhw, zhw = Vector(halfWidth, 0, 0), Vector(0, 0, halfWidth)
-    
+
     local O = BL:copy()
     return {
         C = C,
@@ -239,32 +130,32 @@ local function buildGridPos(g)
         O + (xa * 0.5) - zhw,
         O + (xa * 1.5) - zhw,
         O + (xa * 2.5) - zhw,
-        
+
         O + (xa * 0.5) + (za * 1),
         O + (xa * 1.5) + (za * 1),
         O + (xa * 2.5) + (za * 1),
-        
+
         O + (xa * 0.5) + (za * 2),
         O + (xa * 1.5) + (za * 2),
         O + (xa * 2.5) + (za * 2),
-        
+
         O + (xa * 0.5) + (za * 3) + zhw,
         O + (xa * 1.5) + (za * 3) + zhw,
         O + (xa * 2.5) + (za * 3) + zhw,
-        
+
         -- verticals
         O + (za * 0.5) - xhw,
         O + (za * 1.5) - xhw,
         O + (za * 2.5) - xhw,
-        
+
         O + (xa * 1) + (za * 0.5),
         O + (xa * 1) + (za * 1.5),
         O + (xa * 1) + (za * 2.5),
-        
+
         O + (xa * 2) + (za * 0.5),
         O + (xa * 2) + (za * 1.5),
         O + (xa * 2) + (za * 2.5),
-        
+
         O + (xa * 3) + (za * 0.5) + xhw,
         O + (xa * 3) + (za * 1.5) + xhw,
         O + (xa * 3) + (za * 2.5) + xhw,
@@ -294,15 +185,15 @@ local function buildRoidPos(g)
         g.BL + (xa * (2 / 3)) + (za * 0.5) - xhw,
         g.BL + (xa * (2 / 3)) + (za * 1.5) - xhw,
         g.TL + (xa * (2 / 3)) - (za * 0.5) - xhw,
-        
+
         g.BR - (xa * (2 / 3)) + (za * 0.5) + xhw,
         g.BR - (xa * (2 / 3)) + (za * 1.5) + xhw,
         g.TR - (xa * (2 / 3)) - (za * 0.5) + xhw,
-        
+
         g.BL + (xa * 0.5) + (za * (2 / 3)) - zhw,
         g.BL + (xa * 1.5) + (za * (2 / 3)) - zhw,
         g.BR - (xa * 0.5) + (za * (2 / 3)) - zhw,
-        
+
         g.TL + (xa * 0.5) - (za * (2 / 3)) + zhw,
         g.TL + (xa * 1.5) - (za * (2 / 3)) + zhw,
         g.TR - (xa * 0.5) - (za * (2 / 3)) + zhw,
